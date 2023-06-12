05c7de45fdab86b037a9f736f24c381adcc7e220
Justinas <68568214+JustinasPuzas@users.noreply.github.com>
2023-06-12 03:33:41 +0300

    added start for chrome store reviews

--- src/components/GetTheApp.tsx
+++ src/components/GetTheApp.tsx
@@ -1,37 +1,40 @@
-import Link from 'next/link'
-import styles from './GetTheApp.module.css'
+import Link from "next/link";
+import styles from "./GetTheApp.module.css";
 
-export default function GetTheApp() {  
-    return (
-        <section className={styles.section}>
-            <StoreLink 
-                logo="./chrome.svg"
-                store="Chrome Web Store"
-                link="https://chrome.google.com/webstore/detail/ratepunk-same-hotel-way-c/gdaioanblcnghddimngklkhgcbomfdck?utm_source=ratepunk"
-            />
-            <StoreLink
-                logo="./apple.svg"
-                store="apple app store"
-                link="https://apps.apple.com/app/ratepunk/id1607823726"
-            />
-            <p>Chrome Store reviews</p>
-
-        </section>
-    )
+export default function GetTheApp() {
+  return (
+    <section className={styles.section}>
+      <StoreLink
+        logo="./chrome.svg"
+        store="Chrome Web Store"
+        link="https://chrome.google.com/webstore/detail/ratepunk-same-hotel-way-c/gdaioanblcnghddimngklkhgcbomfdck?utm_source=ratepunk"
+      />
+      <StoreLink
+        logo="./apple.svg"
+        store="apple app store"
+        link="https://apps.apple.com/app/ratepunk/id1607823726"
+      />
+      <div className={styles.stars}>
+        <img src="./star.svg" />
+        <img src="./star.svg" />
+        <img src="./star.svg" />
+        <img src="./star.svg" />
+        <img src="./star.svg" />
+        <p>Chrome Store reviews</p>
+      </div>
+      
+    </section>
+  );
 }
 
-
-
-function StoreLink(props: {logo: string, store: string, link: string}) {
-    return (
-        <Link className={styles.store} href={props.link}>
-            <img src={props.logo}></img>
-            <div>
-                <p>available in the</p>
-                <h4>
-                {props.store}
-                </h4>
-            </div>
-        </Link>
-    )
+function StoreLink(props: { logo: string; store: string; link: string }) {
+  return (
+    <Link className={styles.store} href={props.link}>
+      <img src={props.logo}></img>
+      <div>
+        <p>available in the</p>
+        <h4>{props.store}</h4>
+      </div>
+    </Link>
+  );
 }
